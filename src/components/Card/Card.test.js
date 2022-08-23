1f53bb750a667e8367beae02503166a15f878e7d
Lucas Viana <vianadossantoslucas@gmail.com>
2022-08-23 08:51:24 -0300

    adds tests

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -1,5 +1,54 @@
+import { Text } from "@mantine/core";
+import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Card from "./Card";
+
 describe("Card | component | integration test", () => {
-  it.todo("should display the title when the title prop is passed");
-  test.todo("if children element is showed when the children is passed");
-  it.todo("should call 'onClick' when the show button is clicked");
+  it("should display the title when the title prop is passed", () => {
+    render(
+      <Card title="Card title">
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam
+        </Text>
+      </Card>
+    );
+
+    expect(
+      screen.getByRole("heading", { name: "Card title" })
+    ).toBeInTheDocument();
+  });
+
+  test("if children element is showed when the children is passed", () => {
+    const { rerender } = render(
+      <Card title="Card title">
+        <Text>Lorem ipsum dolor sit amet</Text>
+      </Card>
+    );
+
+    expect(screen.getByText("Lorem ipsum dolor sit amet")).toBeInTheDocument();
+
+    rerender(
+      <Card title="Card title">
+        <Text>Texto atualizado</Text>
+      </Card>
+    );
+
+    expect(screen.getByText("Texto atualizado")).toBeInTheDocument();
+  });
+
+  it("should call 'onClick' when the show button is clicked", () => {
+    const onClickMock = jest.fn();
+
+    render(
+      <Card title="Card title" onClick={onClickMock}>
+        <Text>Lorem ipsum dolor sit amet</Text>
+      </Card>
+    );
+
+    userEvent.click(screen.getByRole("button", { name: "Confirmar" }));
+
+    expect(onClickMock).toHaveBeenCalled();
+  });
 });
